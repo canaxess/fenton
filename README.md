fc1740c4ca064cd4688d46dfca64378182e656f8
rossmullen <43454021+rossmullen@users.noreply.github.com>
2018-09-22 09:03:33 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,81 @@
+# fenton
+fenton provides an accessible UI which has been tested against WCAG 2.0 AA. The script is for bots developed on the Microsoft Bot Framework, the code uses the Directline channel API to communicate with the bot.
+
+Built by following the principles in our [how to make chatbots accessible information card](https://canaxess.com.au/InfoCard/chatbots)
+## Features
+1. Each message in the conversation is accessible from the keyboard using `tabindex`
+2. A non-visual indication of which part of the conversation has been spoken by using the `aria-label` attribute
+3. Accessibility markup for several predefined rich media card types including Hero cards
+4. Markdown support
+## How to create and use fenton
+### Obtain security credentials for your bot:
+1. Make sure you've [registered your bot](https://dev.botframework.com/bots/new)
+2. Add a DirectLine channel, and generate a Direct Line Secret. Make sure Direct Line 3.0 is enabled
+3. Add the Directline secret into the `bot.secret` property
+### Enter your details in the bot object
+```
+var bot = new Object();
+	bot.userId='<userid>';
+	botname='<name>';
+	bot.email='<bot email>';
+	bot.organisation='<organisation>';
+	bot.site='<site>';
+	bot.id='<bot name>';
+	bot.secret='*************************************************';
+```
+### Add the HTML
+Enter the HTML exactly as it is displayed
+```
+<div id="chatbot">
+<div id="description" class="sr-hidden">This is the chatbot conversation window, 
+all conversations are shown in here</div>
+
+<div id="text-container">
+
+<div id="conversation-text-body" aria-labelledby="description" aria-live="polite">
+	<div tabindex="0" aria-label="The bot said" class="conversation-text bot">Hello, how are you?</div>				
+</div>
+</div>
+</div>
+
+<div class="control-container">
+
+<form id="chatbot-form" class="form-inline">
+	<label for="user-msg">Ask me a question</label>
+	<input type="text" autocomplete="off" id="user-msg" class="form-control">
+	<button id="send" type="button" class="button large">Send</button>                     
+</form>
+
+</div>
+
+<button id="connect" type="button" class="button large">Start</button>
+```
+### JQuery usage
+JQuery is used to add click events to Hero cards. All buttons with a class `.actionable` may have a `data-type`, if this has openUrl it will open the page URL in a separate window, else it'll be a response which will carry on the conversation.
+```
+$(function(){
+	$(document).on("click", ".actionable", function(){
+		var obj = $(this);
+		switch(obj.attr('data-type'))
+		{
+			case "openUrl":
+				window.open(obj.attr('data-value'), '_blank');
+				break;
+			default:
+				$('#' + UI.userinputId).val(obj.attr('data-value'));
+				$('#' + UI.submitbuttonId).click();
+				break;
+		}
+	});
+});	
+```
+## Browser and AT support
+The support offered between browsers and assistive technology is increasing and has been tested and passed against the following combinations.
+- JAWS 16 and Internet Explorer 11
+- NVDA 2018.2.1 and Google Chrome
+## External dependencies
+- [RxJS 5.0](https://github.com/ReactiveX/rxjs)
+- [Directline.js](https://github.com/Microsoft/BotFramework-DirectLineJS)
+- JQuery 3.3.1
+- [Showdown 1.8.6](http://showdownjs.com/)
+> This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
---
+++ scripts/rxjs.js
@@ -0,0 +1,194 @@
+var ConnectionStatus = {
+  Uninitialized: 1,
+  Connecting: 2,
+  Online: 3,
+  ExpiredToken: 4,
+  FailedToConnect: 5,
+  Ended: 6,
+};
+
+var startconnection = document.querySelector('#' + UI.connect);
+Rx.Observable.fromEvent(startconnection, 'click')
+	.first()
+	.subscribe(botConnect, err => console.log(err));
+
+var sendmessage = document.querySelector('#' + UI.submitbuttonId);
+const click$ = Rx.Observable.fromEvent(sendmessage, 'click')
+
+click$
+	.bufferWhen(() => click$.delay(1000))
+	.subscribe(sendMessage, err => console.log(err));	
+	
+function buildUserMsg(msg)
+{
+	var HTML = '<div tabindex="0" class="conversation-text user"><span class="sr-hidden">You said </span>' + msg + '</div>';
+	
+	renderResponse(HTML);
+	renderTyping(1);
+}	
+
+function buildBotMessage(msg)
+{
+	var converter = new showdown.Converter(), text = msg, html = converter.makeHtml(text);
+	var output = '<div tabindex="0" class="conversation-text bot"><span class="sr-hidden">The bot said </span>' + html + '</div>';
+	
+	renderTyping(0);
+	renderResponse(output);
+}	
+
+function buildHerocard(response)
+{
+	var text="";
+	var images="";
+	var buttons="";
+		
+	Object.keys(response.attachments[0].content).forEach(function(key)
+	{
+		if(response.attachments[0].content[key]!="")
+		{
+			switch(key)
+			{
+				case "images":
+					for (var i = 0; i < response.attachments[0].content[key].length; i++)
+					{
+						images = images + '<img src="' + response.attachments[0].content[key][i].url + '" alt="">';
+					}
+					break;
+				case "buttons":
+					for (var i = 0; i < response.attachments[0].content[key].length; i++)
+					{
+						buttons = buttons + '<input type="button" class="button actionable" data-type="' + response.attachments[0].content[key][i].type + '" data-value="' + response.attachments[0].content[key][i].value + '" value="' + response.attachments[0].content[key][i].title + '">';
+					}
+					break;
+				default:
+					switch(key)
+					{
+						case "title":
+							text = text + "<div class='heading'>" + response.attachments[0].content[key] + "</div>";
+							break;
+						case "subtitle":
+							text = text + "<div class='subtitle'>" + response.attachments[0].content[key] + "</div>";
+							break;
+						default:
+							text = text + "<div class='text'>" + response.attachments[0].content[key] + "</div>";
+							break;
+					}
+					break;
+			}
+		}
+	})
+	
+	console.log(images);
+	console.log(buttons);
+	console.log(text);
+	
+	var buildString = "<div class='herocard'>";
+	if(images != "")
+	{
+		buildString = buildString + images + "<br>" + text + "<br>" + buttons;
+	}
+	else
+	{
+		buildString = buildString + text + "<br>" + buttons;
+	}
+	buildString = buildString + "</div>";
+	
+	renderTyping(0);
+	var output = '<div tabindex="0" class="conversation-text bot"><span class="sr-hidden">The bot said </span>' + buildString + '</div>';
+	renderResponse(output);
+}
+
+function renderTyping(val)
+{
+	switch(val)
+	{
+		case 0:
+			$(".typing-notification").remove();
+			break;
+		case 1:
+			$("#" + UI.conversationId).append('<div class="typing-notification conversation-text bot"><span class="sr-hidden">The bot is replying </span><img src="./images/loading.gif" class="bot-typing"></div>');
+			document.getElementById(UI.conversationContainerId).scrollTop = 4500;
+			break;
+	}
+}
+	
+function renderResponse(msg)
+{
+	$('#' + UI.conversationId).append(msg);
+	document.getElementById(UI.conversationContainerId).scrollTop = 4500;
+}
+	
+function sendMessage()
+{
+	if(directLine!=undefined)
+	{
+		directLine.postActivity({
+			from: { id: bot.userId, name: bot.name },
+			type: 'message',
+			text: $('#' + UI.userinputId).val()
+			}).subscribe(
+			buildUserMsg($('#' + UI.userinputId).val()),
+			error => console.log("Error posting activity", error)
+		);
+	}
+}
+	
+function botConnect()
+{
+	directLine = new DirectLine.DirectLine({
+		secret: bot.secret,
+		domain: 'https://directline.botframework.com/v3/directline/',
+		webSocket: true,
+		pollingInterval: 1000,
+	});
+	
+	renderTyping(1);
+	
+	directLine.activity$
+	.filter(activity => activity.from.id === bot.id)
+	.subscribe(activity =>
+	{
+		if(activity.attachments.length>0)
+		{
+			var contentType = activity.attachments[0].contentType;
+			switch(contentType)
+			{
+				case "application/vnd.microsoft.card.thumbnail":
+				case "application/vnd.microsoft.card.hero":
+					buildHerocard(activity);
+					break;
+			}
+		}
+		else
+		{
+			buildBotMessage(activity.text);
+		}
+	})
+		
+	directLine.connectionStatus$
+	.subscribe(connectionStatus =>{
+		switch(connectionStatus) {
+			case ConnectionStatus.Uninitialized:
+				console.log('ConnectionStatus.Uninitialized')
+				break;
+			case ConnectionStatus.Connecting:
+				console.log('ConnectionStatus.Connecting')
+				break;
+			case ConnectionStatus.Online:
+				console.log('ConnectionStatus.Online')
+				break;
+			case ConnectionStatus.ExpiredToken:
+				console.log('ConnectionStatus.ExpiredToken')
+				break;
+			case ConnectionStatus.FailedToConnect:
+				console.log('ConnectionStatus.FailedToConnect')
+				break;
+			case ConnectionStatus.Ended:
+				console.log('ConnectionStatus.Ended')
+				break;
+			default:
+				console.log("other code")
+				break;
+		}
+	});
+}
